ec450f944af12394f7f03cee910207853a035829
areeba-m <areebabrown@gmail.com>
2026-07-24 17:21:53 +0500

    minor package changes

--- spring-ai/src/main/java/com/redmath/Main.java
+++ spring-ai/src/main/java/com/redmath/Main.java
@@ -1,4 +1,4 @@
-package com.redmath.lecture02;
+package com.redmath;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-ai/src/main/java/com/redmath/chat/ChatConfiguration.java
+++ spring-ai/src/main/java/com/redmath/chat/ChatConfiguration.java
@@ -1,4 +1,4 @@
-package com.redmath.lecture02.chat;
+package com.redmath.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
